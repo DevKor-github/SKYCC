5d10e2ddcb2560cc8dd324b45f684772e275cec9
overthestream <njhnjh02@naver.com>
2023-05-14 00:35:44 +0900

    lint

--- backend/src/chatgpt/chatgpt.service.spec.ts
+++ backend/src/chatgpt/chatgpt.service.spec.ts
@@ -2,7 +2,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ChatGptService } from './chatgpt.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-
 describe('ChatGptService', () => {
   let service: ChatGptService;
   let configService: ConfigService;
@@ -10,9 +9,7 @@ describe('ChatGptService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [ConfigModule.forRoot()],
-      providers: [
-        ChatGptService, 
-        ],
+      providers: [ChatGptService],
     }).compile();
 
     service = module.get<ChatGptService>(ChatGptService);
@@ -20,10 +17,10 @@ describe('ChatGptService', () => {
   });
 
   it('should be defined', async () => {
-    const prompt = '5월 23일 오전 8시 서울에서 부산으로 가는 버스를 예약해줘'
+    const prompt = '5월 23일 오전 8시 서울에서 부산으로 가는 버스를 예약해줘';
     const completion = await service.complete(prompt);
-    console.log(typeof completion)
-    console.log(completion)
+    console.log(typeof completion);
+    console.log(completion);
     expect(completion).toBeDefined();
   });
 });
