081ef91cbdc8f82c9aef08d71ca46c933b2f7028
ryuni-dev <ryuni.dev@gmail.com>
2023-05-14 00:32:57 +0900

    feat: test code 작성

--- backend/src/stt/stt.service.spec.ts
+++ backend/src/stt/stt.service.spec.ts
@@ -1,6 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SttService } from './stt.service';
 
+const params = {
+  TranscriptionJobName: "test",
+  LanguageCode: "ko-KR", // For example, 'en-US'
+  MediaFormat: "m4a", // For example, 'wav'
+  Media: {
+      MediaFileUri: "",
+    // For example, "https://transcribe-demo.s3-REGION.amazonaws.com/hello_world.wav"
+  },
+  OutputBucketName: "test"
+};
+
 describe('SttService', () => {
   let service: SttService;
 
@@ -12,7 +23,9 @@ describe('SttService', () => {
     service = module.get<SttService>(SttService);
   });
 
-  it('should be defined', () => {
+  it('should be defined', async () => {
+    const result = await service.startTranscriptionJob(params)
+    console.log(result)
     expect(service).toBeDefined();
   });
 });
--- backend/src/stt/stt.service.ts
+++ backend/src/stt/stt.service.ts
@@ -1,21 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { StartTranscriptionJobCommand, TranscribeClient, StartTranscriptionJobCommandOutput, StartTranscriptionJobCommandInput } from '@aws-sdk/client-transcribe';
 
-export const params = {
-    TranscriptionJobName: "JOB_NAME",
-    LanguageCode: "LANGUAGE_CODE", // For example, 'en-US'
-    MediaFormat: "mp4", // For example, 'wav'
-    Media: {
-        MediaFileUri: "SOURCE_LOCATION",
-      // For example, "https://transcribe-demo.s3-REGION.amazonaws.com/hello_world.wav"
-    },
-    OutputBucketName: "OUTPUT_BUCKET_NAME"
-};
-
 @Injectable()
 export class SttService {
     private readonly transcribeClient: TranscribeClient;
-    private readonly REGION = "REGION";
+    private readonly REGION = "ap-northeast-2";
     constructor() {
         this.transcribeClient = new TranscribeClient({ region: this.REGION });
     }
